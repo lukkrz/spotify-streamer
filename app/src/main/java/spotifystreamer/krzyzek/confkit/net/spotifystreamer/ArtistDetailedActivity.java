136adeb83f31ebbcebb0a8805316ac998399ada2
lukkrz <lukasz.krzyzek@gmail.com>
2015-06-23 23:01:05 +0200

    Fix onOptionsItemSelected

--- app/src/main/java/spotifystreamer/krzyzek/confkit/net/spotifystreamer/ArtistDetailedActivity.java
+++ app/src/main/java/spotifystreamer/krzyzek/confkit/net/spotifystreamer/ArtistDetailedActivity.java
@@ -1,6 +1,7 @@
 package spotifystreamer.krzyzek.confkit.net.spotifystreamer;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,16 +26,12 @@ public class ArtistDetailedActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
